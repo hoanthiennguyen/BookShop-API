c425cbc3499e694437bc2b370219fc813606ee9f
hoanthien <hoanthien97@gmail.com>
2019-10-15 15:06:09 +0700

    change type of response in cart controller

--- src/main/java/app/controller/CartController.java
+++ src/main/java/app/controller/CartController.java
@@ -29,16 +29,22 @@ public class CartController {
         return cartService.addItem(authentication.getName(),order);
     }
     @PutMapping("/{id}")
-    public List<CartItem> editItem(@AuthenticationPrincipal Authentication authentication, @PathVariable Long id, @RequestBody ChangeQuantityInCartItemRequest quantity){
-        return cartService.editItem(authentication.getName(),id,quantity.getQuantity());
+    public BaseResponse editItem(@AuthenticationPrincipal Authentication authentication, @PathVariable Long id, @RequestBody ChangeQuantityInCartItemRequest quantity){
+        BaseResponse response = new BaseResponse();
+        response.setData(cartService.editItem(authentication.getName(),id,quantity.getQuantity()));
+        return response;
     }
     @DeleteMapping("/{id}")
-    public List<CartItem> deleteItem(@AuthenticationPrincipal Authentication authentication, @PathVariable Long id){
-        return cartService.deleteItem(authentication.getName(),id);
+    public BaseResponse deleteItem(@AuthenticationPrincipal Authentication authentication, @PathVariable Long id){
+        BaseResponse response = new BaseResponse();
+        response.setData(cartService.deleteItem(authentication.getName(),id));
+        return response;
     }
     @PostMapping("/pay")
-    public Bill makePay(@AuthenticationPrincipal Authentication authentication){
-        return cartService.pay(authentication.getName());
+    public BaseResponse makePay(@AuthenticationPrincipal Authentication authentication){
+        BaseResponse response = new BaseResponse();
+        response.setData(cartService.pay(authentication.getName()));
+        return response;
 
     }
 }
